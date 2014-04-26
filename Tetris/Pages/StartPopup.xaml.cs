1f7e2f2c617c8428b89dbfc41a65802cb833a862
dbtds <dbtdsilva@gmail.com>
2014-04-26 15:26:20 +0100

    Added images to buttons based on PathGeometry

--- Tetris/Pages/StartPopup.xaml.cs
+++ Tetris/Pages/StartPopup.xaml.cs
@@ -30,5 +30,10 @@ namespace Tetris.Pages
             TetrisM.getInstance().startGame();
             MainWindow.Instance.exitPopup();
         }
+
+        private void KinectCircleButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
